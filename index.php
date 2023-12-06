988680bb483e394c20654ecf44a08fee762b158d
Macieklabuz <xnarhice@gmail.com>
2023-12-06 17:38:19 +0100

    data base

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Witam witam o tej porze kazdy wypic moze';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Routing::get('', 'DefaultController');
+Routing::get('login', 'DefaultController');
+
+Routing::run($path);
